efa70fb87a3973f69e892ee4c61a205764ffc047
Dobando <dobando@wearegap.com>
2020-11-17 02:15:50 +0000

    Ejercicio completo

--- kbot.rb
+++ kbot.rb
@@ -1,1 +1,17 @@
-puts "hola, mundo"
+class KleerBot
+    def initialize(idioma)
+        @idioma = idioma
+	end
+		
+    def saludar nombre
+        if(@idioma == "EN")
+            "Hello #{nombre}! \nHow can I help you?"
+        else
+            "Hola #{nombre}! \n¿Cómo puedo ayudarte?"
+        end
+    end
+end
+
+kleer = KleerBot.new(ARGV[1])
+
+puts kleer.saludar(ARGV[0])
